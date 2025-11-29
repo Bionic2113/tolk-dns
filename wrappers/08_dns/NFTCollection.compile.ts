4760c8acfe88daf1ff3fd9899a22285a30300f0a
Bionic2113 <lol_smail@bk.ru>
2025-11-29 16:43:50 +0500

    feat: tolk v1.1

--- wrappers/08_dns/NFTCollection.compile.ts
+++ wrappers/08_dns/NFTCollection.compile.ts
@@ -1,11 +1,11 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['stdlib.fc', 'params.fc', 'op-codes.fc', 'dns-utils.fc', 'nft-collection.fc'],
-    debugInfo: true,
-    // lang: 'tolk',
-    // entrypoint: 'nft-collection.tolk',
-    // withSrcLineComments: true,
-    // withStackComments: true,
+    // lang: 'func',
+    // targets: ['stdlib.fc', 'params.fc', 'op-codes.fc', 'dns-utils.fc', 'nft-collection.fc'],
+    // debugInfo: true,
+    lang: 'tolk',
+    entrypoint: 'nft-collection.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 };
--- wrappers/08_dns/NFTItem.compile.ts
+++ wrappers/08_dns/NFTItem.compile.ts
@@ -1,11 +1,11 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['stdlib.fc', 'params.fc', 'op-codes.fc', 'dns-utils.fc', 'nft-item.fc'],
-    debugInfo: true,
-    // lang: 'tolk',
-    // entrypoint: 'nft-item.tolk',
-    // withSrcLineComments: true,
-    // withStackComments: true,
+    // lang: 'func',
+    // targets: ['stdlib.fc', 'params.fc', 'op-codes.fc', 'dns-utils.fc', 'nft-item.fc'],
+    // debugInfo: true,
+    lang: 'tolk',
+    entrypoint: 'nft-item.tolk',
+    withSrcLineComments: true,
+    withStackComments: true,
 };
